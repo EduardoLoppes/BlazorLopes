391bc6b2cc949f575c8599f6d6ac78db54b219a0
EduardoLoppes <mcfgonzalles@gmail.com>
2022-06-10 11:28:41 -0300

    Adicionar arquivos de projeto.

---
+++ BlazorLopes/Client/Program.cs
@@ -0,0 +1,15 @@
+
+using Blazored.Toast;
+using BlazorLopes.Client;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddBlazoredToast();
+
+
+await builder.Build().RunAsync();
---
+++ BlazorLopes/Server/Features/Cep/Controllers/CepController.cs
@@ -0,0 +1,35 @@
+﻿using BlazorLopes.Shared.Features.Cep.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace BlazorLopes.Server.Features.Cep.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CepController : ControllerBase
+    {
+        [HttpGet("{cep}")]
+        public Endereco RetornarEndereco(string cep)
+        {
+            var endereco = new Endereco();
+            
+            using (var http = new HttpClient())
+            {
+                http.BaseAddress = new Uri("https://viacep.com.br/ws/");
+
+                try
+                {
+                    var json = http.GetStringAsync($"{cep}/json");
+                    endereco = JsonConvert.DeserializeObject<Endereco>(json.Result);
+                }
+                catch
+                {
+                    endereco.erro = true;
+                }
+                
+            }
+            
+            return endereco;
+        }
+    }
+}
